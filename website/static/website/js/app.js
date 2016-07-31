b4cacf5ed3e4bd98a693cddb2ba48674add19cb6
Christophe Blefari <cblefari@equancy.com>
2016-07-31 17:47:48 +0200

    Put a connected visual help

--- website/static/website/js/app.js
+++ website/static/website/js/app.js
@@ -7,12 +7,25 @@ import Messages from "./components/Messages";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: chance.name(), messages: [] }
+    this.state = { user: chance.name(), messages: [], connected: false }
   }
 
   componentDidMount() {
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     this.chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+
+    this.chatsock.onopen = event => {
+      this.setState({
+        connected: true
+      })
+      console.log(this.chatsock);
+    };
+
+    this.chatsock.onclose = event => {
+      this.setState({
+        connected: false
+      })
+    };
 
     this.chatsock.onmessage = (message) => {
       console.log(message);
@@ -33,7 +46,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>M O O N Y</h1>
+        <h1>M O O N Y (<span>{this.state.connected ? 'connected' : 'disconnected'}</span>)</h1>
         <Messages
           messages={this.state.messages}
         />
