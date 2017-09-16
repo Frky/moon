1b22e6b9a50d63533864ebf066283581737fcb15
Christophe Blefari <christophe.blefari@gmail.com>
2017-09-16 12:05:15 +0200

    Change local_sample.py for deployement

--- moon/settings/local_sample.py
+++ moon/settings/local_sample.py
@@ -1,3 +1,8 @@
 from moon.settings.base import *
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '1') == '1'
+DEV = os.getenv('DEV', '1') == '1'
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.getenv('STATIC_ROOT', '/app/static')
